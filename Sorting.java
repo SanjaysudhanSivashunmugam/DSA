ca1702c1d24b00bca50eaa4e44a10aac4c2c42c3
SanjaysudhanSivashunmugam <sanjaysudhan.sivashunmugam@gmail.com>
2026-08-18 09:01:35 +0530

    DSA - Sorting

--- Sorting.java
+++ Sorting.java
@@ -1,51 +1,45 @@
-import java.util.*;
-class Sorting {
-    public static void main(String[] args) {
-        int arr[] = {8, 5, 9, 3, 1, 0, 2};
-        mergeSort(arr, 0, arr.length - 1);
-        for(int i : arr) {
-            System.out.print(i + " ");
-        }
-    }
-
-    public static void mergeSort(int arr[], int low, int high) {
-        if(low == high) {
-            return;
-        }
-        int mid = (low + high) / 2;  
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-        merge(arr, low, mid, high);
-    }
-
-    public static void merge(int arr[], int low, int mid, int high) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        int i = low;
-        int j = mid + 1;
-        while (i <= mid && j <= high) {
-            if(arr[i] < arr[j]) {
-                temp.add(arr[i]);
-                i++;
-            }
-
-            else {
-                temp.add(arr[j]);
-                j++;
-            }
-        }
-
-        while (i <= mid) {
-            temp.add(arr[i]);
-            i++;
-        }
-    
-        while (j <= high) {
-            temp.add(arr[j]);
-            j++;
-        }
-
-        for(int k = low; k <= high; k++) {
-            arr[k] = temp.get(k - low);
-        }
-    }
+public class Sorting
+{
+	public static void main(String[] args) {
+		int arr[] = {4, 2, 1, 5, 3};
+		quickSort(arr, 0, arr.length - 1);
+		for(int i : arr) {
+		    System.out.println(i);
+		}
+	}
+	
+	static void quickSort(int arr[], int low, int high) {
+	    if(low < high) {
+	        int pivot = fun(arr, low, high);
+	        quickSort(arr, low, pivot - 1);
+	        quickSort(arr, pivot + 1, high);
+	    }
+	}
+	
+	static int fun(int arr[], int low, int high) {
+	    int pivot = low;
+	    int i = low;
+	    int j = high;
+	    while(i < j) {
+	        while(i < high && arr[pivot] >= arr[i]) {
+	            i++;
+	        }
+	        
+	        while(j > low && arr[pivot] < arr[j]) {
+	            j--;
+	        }
+	        
+	        if(i < j) {
+	            int temp = arr[i];
+	            arr[i] = arr[j];
+	            arr[j] = temp;
+	        }
+	    }
+	    
+	    int temp = arr[pivot];
+	    arr[pivot] = arr[j];
+	    arr[j] = temp;
+	    
+	    return j;
+	}
 }
